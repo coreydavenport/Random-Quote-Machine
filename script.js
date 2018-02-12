a0a23d1b0dd4902286da2c37c1aaf2e521634336
Corey Davenport <coreydavenport@users.noreply.github.com>
2018-02-12 16:01:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+  var getQuote = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+  
+  
 
+  
+  
+    $.getJSON(getQuote)
+      .done(function(init){
+      $("#quote").html(JSON.stringify(init["quoteText"]));
+      $("#author").html(JSON.stringify(init["quoteAuthor"]).replace(/\"/g, ""));
+    })
+  
+  
+    $(".btn").click(function(){
+      $.getJSON(getQuote)
+      .done(function(data){
+        $("#quote").html(JSON.stringify(data["quoteText"]));
+        $("#author").html(JSON.stringify(data["quoteAuthor"]).replace(/\"/g, ""));
+      })
+    });
 });
