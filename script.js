2557d21fa9697f3dd5fb74f1e16b3f7e8d04cb4c
Corey Davenport <coreydavenport@users.noreply.github.com>
2018-02-13 09:56:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 $(document).ready(function(){
   var getQuote = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
-  
+
   
 
   
   
     $.getJSON(getQuote)
       .done(function(init){
-      $("#quote").html(JSON.stringify(init["quoteText"]));
+      var quote = JSON.stringify(init["quoteText"]);
+      
+      $("#quote").html(quote);
       $("#author").html(JSON.stringify(init["quoteAuthor"]).replace(/\"/g, ""));
-    })
-  
-  
-    $(".btn").click(function(){
+      $(".btn").click(function(){
       $.getJSON(getQuote)
-      .done(function(data){
-        $("#quote").html(JSON.stringify(data["quoteText"]));
-        $("#author").html(JSON.stringify(data["quoteAuthor"]).replace(/\"/g, ""));
+      .done(function(init){
+        $("#quote").html(JSON.stringify(init["quoteText"]));
+        $("#author").html(JSON.stringify(init["quoteAuthor"]).replace(/\"/g, ""));
       })
     });
+     
+      $("#tweetQuote").click(function(){
+        var quoteTweet = $("#quote").text().replace(/\"/g, "");
+        var quoteAuthor= $("#author").text();
+        var message = quoteTweet + " - " + quoteAuthor;
+        
+        if(message.length < 140){
+          window.open('https://twitter.com/intent/tweet?text=' + message + '', '_blank');
+        };
+        
+      });
+      
+    });
+  
+  
+
+
+  
+  
 });
